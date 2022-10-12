1788472088edb27995b3aa4bb30fedbb0bcce3d8
Matthewlisd <60634784+Matthewlisd@users.noreply.github.com>
2022-10-12 17:25:58 -0400

    Add files via upload

---
+++ src/Create.java
@@ -0,0 +1,101 @@
+package src;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Create extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JLabel lblNewLabel_1;
+	private JTextField textField_1;
+	private JLabel lblNewLabel_2;
+	private JTextField textField_2;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Create frame = new Create();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Create() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 655);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Recipe Name:");
+		lblNewLabel.setBounds(32, 30, 109, 38);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(143, 30, 318, 29);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		lblNewLabel_1 = new JLabel("Ingridients:");
+		lblNewLabel_1.setBounds(32, 79, 109, 29);
+		contentPane.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(143, 79, 318, 29);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		lblNewLabel_2 = new JLabel("Steps:");
+		lblNewLabel_2.setBounds(32, 119, 109, 29);
+		contentPane.add(lblNewLabel_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(143, 119, 326, 425);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		btnNewButton = new JButton("Submit");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = textField.getText();
+				String ingridients = textField_1.getText();
+				String steps = textField_2.getText();
+				//* Store into Data Base*//
+			}
+		});
+		btnNewButton.setBounds(143, 586, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		btnNewButton_1 = new JButton("HOME");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new Cover().setVisible(true);
+				Create.this.dispose();
+			}
+		});
+		btnNewButton_1.setBounds(380, 586, 89, 23);
+		contentPane.add(btnNewButton_1);
+	}
+}
